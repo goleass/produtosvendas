ddfdeec817567714e35c80ab4d07b7788d7c862c
goleass <leonardo-assuncao1@hotmail.com>
2024-10-16 00:10:08 -0300

    Fix

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/receita_teste_1/assets', express.static(path.join(__dirname, 'public/receita_teste_1/assets')));
-
-// Servindo o arquivo HTML na rota "/"
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public','receitas_bolo_01', 'index.html'));
-});
+app.use('/receitas_recheios_sem_fogo/assets', express.static(path.join(__dirname, 'public/receitas_recheios_sem_fogo/assets')));
 
 app.get('/receitas-para-bebes/pagamento', (req, res) => {
   res.redirect('https://go.hotmart.com/S96032525D?ap=a166');
@@ -22,7 +18,7 @@ app.get('/receitas-para-bebes', (req, res) => {
 });
 
 app.get('/receitas-que-nao-vao-fogo', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public','receitas_bolo_01', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public','receitas_recheios_sem_fogo', 'src', 'index.html'));
 });
 
 app.get('/curso-simplesmente-amigurumis-4-0', (req, res) => {
